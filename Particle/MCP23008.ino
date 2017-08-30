58f60660c832bd2f6bf389c7ae5548eeafe17058
zaphod-42 <trey@tomadoh.com>
2017-08-30 15:10:47 -0500

    Update MCP23008.ino

--- Particle/MCP23008.ino
+++ Particle/MCP23008.ino
@@ -1,20 +1,39 @@
 // This #include statement was automatically added by the Particle IDE.
 #include "MCP23008.h"
 MCP23008 relayController;
+MCP23008 relayController33;
 
-String relayStatus;
-String inputStatus;
+int relayStatus;
+int inputStatus;
 
 void setup() {
-    relayController.setOutputs(4);
+    
+    relayController.setRelays(4);
+    relayController.setInputs(240);
+    relayController.init();
+    
+    relayController33.setAddress(1,0,0);
+    relayController33.setRelays(8);
+    relayController33.init();
+    
     relayController.publishInputEvents = true;
-    Particle.function("relayControl", &MCP23008::relayTalk, &relayController);
+    Particle.function("control32", &MCP23008::relayTalk, &relayController);
+    Particle.function("control33", &MCP23008::relayTalk, &relayController33);
     Particle.variable("relayStatus", relayStatus);
     Particle.variable("inputStatus", inputStatus);
     
 }
 void loop() {
-    relayStatus = String(relayController.readRelayBankStatus());
     
-    inputStatus = String(relayController.readAllInputs());
+    inputStatus = relayController.readAllInputs();
+    
+    //Tie input to output (4 is input 3 counting from 0)
+    if((inputStatus & 4) > 0){
+        relayController33.turnOnRelay(2);
+    }else{
+        relayController33.turnOffRelay(2);
+    }
+    
+    relayStatus = (relayController.readRelayBankStatus() << 8) + relayController33.readRelayBankStatus();
+    
 }
