956e0a6ed89993783b77c34db4507b16d4f6da68
io-Jacob <jcy.youngblood@gmail.com>
2022-07-22 11:21:06 -0500

    Update Simple-GPIO-Monitor.py

--- Python/Examples/Simple-GPIO-Monitor.py
+++ Python/Examples/Simple-GPIO-Monitor.py
@@ -16,7 +16,9 @@ bus = smbus.SMBus(1)
 # kwargs = {'address': 0x20, 'gpio_output_map': gpio_output_map}
 #create the MCP23008 object from the MCP23008 library
 #the object requires that you pass it the bus object so that it can communicate and share the bus with other chips if necessary
-mcp23008 = mcp23008(smbus)
+mcp23008 = mcp23008(bus)
+#to pass in kwargs you should instantiate the mcp23008 object with the following line
+#mcp23008 = mcp23008(bus, kwargs)
 
 #by default the inputs are floating. I want to pull up input GPIO 4 so I can hook up a simple switch and read its status.
 mcp23008.pull_up_gpio(4)
@@ -30,7 +32,7 @@ while True :
         #print out the byte that indicates the status of the GPIOs.
         #255 means all GPIOs are high. 0 means all GPIOs are low.
         #if only GPIO 0 is high then the value will be 1, if only GPIO 3 is high the value will be 8
-        #the bits in the byte would look like 00000001 (GPIO 0 is 1 aka high) and 00001000 (GPIO 0 is 1 aka high)
+        #the bits in the byte would look like 00000001 (GPIO 0 is 1 aka high) and 00001000 (GPIO 3 is 1 aka high)
         print mcp23008.get_all_gpio_status()
 
     
